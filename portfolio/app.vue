8164ffc4430869c2ce256e6e7097c2275ea64e59
Fouche du Preez <fouche@limehouse.co.za>
2023-11-25 18:39:45 +0200

    init

---
+++ portfolio/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <NuxtLink to="/">Main</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/about">About</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
