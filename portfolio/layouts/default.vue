4d3cee88c322b30555c394f9a81ce0a0e23b6a0a
Fouche du Preez <fouche@limehouse.co.za>
2023-11-26 10:58:13 +0200

    added tailwind and basic components

--- portfolio/layouts/default.vue
+++ portfolio/layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/">Main</NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about">About</NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <slot />
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div>
+        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200">Fouche du Preez</NuxtLink>
+      </div>
+      <Menu />
+    </header>
+    <main class="p-2">
+      <slot />
+    </main>
   </div>
 </template>
+<script setup lang="ts">
+</script>
